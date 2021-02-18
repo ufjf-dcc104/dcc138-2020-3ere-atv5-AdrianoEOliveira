8a894012098680595dd7664f9e3d3a2c1c4abd07
Adriano Eiterer Oliveira <Adrianoeo@outlokk.com>
2021-02-18 17:54:22 -0300

    Adiciona lista de sprites na cena

--- js/Main.js
+++ js/Main.js
@@ -2,12 +2,13 @@ import Cena from"./Cena.js"
 import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 const cena1= new Cena(canvas);
-cena1.desenhar();
-
 const pc = new Sprite({});
 const en1 = new Sprite({x:140,y:100,w:10,h:10,color:"red"});
+cena1.adicionarSprite(pc);
+cena1.adicionarSprite(en1);
 
-pc.desenhar(ctx);
-en1.desenhar(ctx);
+cena1.desenhar();
+
+
+
