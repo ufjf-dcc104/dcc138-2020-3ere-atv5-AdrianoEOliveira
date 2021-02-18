0b29dc303b24b0db3218fff6cc9d6689a2c1bb20
Adriano Eiterer Oliveira <Adrianoeo@outlokk.com>
2021-02-18 17:10:42 -0300

    Cria e usa uma classe de cena

---
+++ js/Cena.js
@@ -0,0 +1,14 @@
+export default class Cena
+{
+    /* E responsável por desenhar elementos na tela de uma animação
+    */
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+    }
+    desenhar()
+    {
+        this.fillStyle = "black";
+        this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+    }
+}
