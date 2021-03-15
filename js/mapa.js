ebfd3c78cd76063228fd0621477f6f08d1cf588b
Adriano Eiterer Oliveira <Adrianoeo@outlokk.com>
2021-03-14 21:26:43 -0300

    Adiciona carregamento de tile por imagens

--- js/mapa.js
+++ js/mapa.js
@@ -1,6 +1,6 @@
 export default class Mapa
 {
-    constructor(linhas=8,colunas = 12 ,tamanho=32)
+    constructor(linhas=15,colunas = 15 ,tamanho=32)
     {
         this.LINHAS = linhas;
         this.COLUNAS= colunas;
@@ -13,31 +13,27 @@ export default class Mapa
             {
             this.tiles[l][c] = 0;
             }
-        this.cena =null;
-
+        this.cena = null;
         }
     }
     desenhar(ctx)
     {
+        let img =new Image();
+        img = this.cena.assets.Img("terreno");
+        let linha = 21;
+        let coluna = 8;
         for (let l = 0; l < this.LINHAS; l++) 
         {
             for (let c = 0; c < this.COLUNAS; c++) 
             {
-                switch(this.tiles[l][c])
-                {
-                case 1:
-                ctx.fillStyle = "Grey";
-                ctx.lineWidth = 1;
-                ctx.strokeStyle = "red";
-                break;
-                default:
-                    ctx.fillStyle = "black";
-                    ctx.lineWidth = 1;
-                    ctx.strokeStyle = "cyan";
-                   
-                    break;
-                }
-                ctx.fillRect(c*this.TAMANHO,l*this.TAMANHO,this.TAMANHO,this.TAMANHO);
+
+                ctx.drawImage(img,coluna * 32,linha *32,32,32,
+                    c*32,l*32,32,32);
+                    if(this.tiles[l][c]==1)
+                    {
+                        ctx.drawImage(img,21 * 32,21 *32,32,32,
+                            c*32,l*32,32,32);
+                    }
                 ctx.strokeRect(c*this.TAMANHO,l*this.TAMANHO,this.TAMANHO,this.TAMANHO);
             }
         }
